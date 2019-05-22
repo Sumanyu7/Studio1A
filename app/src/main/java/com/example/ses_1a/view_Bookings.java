55530cd8ffae7fa38fce1bd07abe6498cd48f4a2
sumanyu <sumanyu1711@gmail.com>
2019-05-22 17:02:20 +1000

    viewBookings

--- app/src/main/java/com/example/ses_1a/view_Bookings.java
+++ app/src/main/java/com/example/ses_1a/view_Bookings.java
@@ -4,13 +4,37 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.util.Log;
+import android.widget.Toast;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.ArrayList;
 
 public class view_Bookings extends AppCompatActivity {
+
+    private static final String TAG = "view_Booking";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private RecyclerView data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +42,7 @@ public class view_Bookings extends AppCompatActivity {
         setContentView(R.layout.activity_view__bookings);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +52,45 @@ public class view_Bookings extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        viewBooking();
+
     }
 
+    protected void viewBooking() {
+        super.onStart();
+
+        data = findViewById(R.id.viewData);
+
+        db.collection("bookings")
+                .whereEqualTo("donorEmail", "sumanyu@gmail.com")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        List<String> bookings = new ArrayList<>();
+                        for (QueryDocumentSnapshot doc : value) {
+                            if (doc.get("charityName") != null) {
+                                bookings.add(doc.getString("charityName"));
+                            }
+                            if (doc.get("date") != null) {
+                                bookings.add(doc.getString("date"));
+                            }
+                            if (doc.get("time") != null) {
+                                bookings.add(doc.getString("time"));
+                            }
+
+                        }
+                        Log.d(TAG,"Bookings" + bookings);
+                    }
+
+                });
+
+
+    }
 }
